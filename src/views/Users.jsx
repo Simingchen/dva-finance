32912d0d7a096812ebaae9cca01e01637e0a1d5e
Siming <siming535@gmail.com>
2019-07-31 14:12:34 +0800

    Create Users.jsx

---
+++ src/views/Users.jsx
@@ -0,0 +1,52 @@
+// ./src/routes/Users.jsx
+import React  from 'react';
+
+// Users 的 Presentational Component
+// 暂时都没实现
+import UserList from '../components/Users/UserList';
+import UserSearch from '../components/Users/UserSearch';
+import UserModal from '../components/Users/UserModal';
+
+// 引入对应的样式
+// 可以暂时新建一个空的
+import styles from '../css/Users.less';
+
+function Users() {
+  const userSearchProps={};
+  const userListProps={
+    total: 3,
+    current: 1,
+    loading: false,
+    dataSource: [
+      {
+        name: '张三',
+        age: 23,
+        address: '成都',
+      },
+      {
+        name: '李四',
+        age: 24,
+        address: '杭州',
+      },
+      {
+        name: '王五',
+        age: 25,
+        address: '上海',
+      },
+    ],
+  };
+  const userModalProps={};
+
+  return (
+    <div className={styles.normal}>
+      {/* 用户筛选搜索框 */}
+      <UserSearch {...userSearchProps} />
+      {/* 用户信息展示列表 */}
+      <UserList {...userListProps} />
+      {/* 添加用户 & 修改用户弹出的浮层 */}
+      <UserModal {...userModalProps} />
+    </div>
+  );
+}
+
+export default Users;
