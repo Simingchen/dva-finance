3d3b61f9f3d7b46a38154417187660f52a42c03e
Siming <siming535@gmail.com>
2019-07-31 10:19:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dva from "dva";
 import "./index.css";
-
+const router = require("./router")["default"]
+console.log(router)
 // 1. Initialize
 const app = dva();
 
@@ -11,7 +12,7 @@ const app = dva();
 // app.model(require('./models/example').default);
 
 // 4. Router
-app.router(require("./router")["default"]);
+app.router(router);
 
 // 5. Start
 app.start("#root");
