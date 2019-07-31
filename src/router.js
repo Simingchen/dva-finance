66f454a25e85d1aa8c4e9920fc61fd9d8344a342
Siming <siming535@gmail.com>
2019-07-31 14:00:24 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Router, Route, Switch } from "dva/router";
-import IndexPage from "./routes/IndexPage";
+import IndexPage from "./routes/IndexPage.jsx";
 import Users from './routes/Users';
 
 function RouterConfig({history}) {
