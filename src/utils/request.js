a621881534bd4195702abf1402218a618e09d1da
Simingchen <siming535@gmail.com>
2019-08-02 14:41:07 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,21 @@
 import fetch from 'dva/fetch';
+import { getToken } from './auth'
+
 
 function parseJSON(response) {
+  console.log(response)
   return response.json();
 }
 
 function checkStatus(response) {
+  console.log(response.status)
   if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
 
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
+  }
+  return response;
+  // const error = new Error(response.statusText);
+  // error.response = response;
+  // throw error;
 }
 
 /**
@@ -21,10 +25,29 @@ function checkStatus(response) {
  * @param  {object} [options] The options we want to pass to "fetch"
  * @return {object}           An object containing either "data" or "err"
  */
-export default function request(url, options) {
-  return fetch(url, options)
+
+const baseUrl = 'http://47.107.46.219:8083/'
+
+function request(options = {}) {
+  const config = {
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    mode: 'no-cors',
+  }
+  config.headers['cache-control'] = 'no-cache'
+  config.headers['Pragma'] = 'no-cache'
+  config.headers['token'] = getToken() // 让每个请求携带自定义token
+
+  options = { ...options, ...config}
+  return fetch(baseUrl + options.url, options)
     .then(checkStatus)
     .then(parseJSON)
     .then(data => ({ data }))
     .catch(err => ({ err }));
 }
+
+
+
+export default request
