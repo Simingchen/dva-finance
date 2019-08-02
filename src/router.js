a3ec8b38e97647d8df49d2f73671e1df1027931b
Simingchen <siming535@gmail.com>
2019-08-02 14:41:34 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Router, Route, Switch } from "dva/router";
 import IndexPage from "./views/IndexPage.jsx";
 import Users from './views/Users.jsx';
+import login from './views/login.jsx';
 
 function RouterConfig({history}) {
     return (
         <Router history={history}>
             <Switch>
                 <Route path="/" exact component={IndexPage} />
+                <Route path="/login" component={login} />
                 <Route path="/users" component={Users} />
             </Switch>
         </Router>
