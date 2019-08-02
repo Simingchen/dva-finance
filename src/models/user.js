cc80b0013ac4014b7f43c829e5adae7a9ae7bf34
Simingchen <siming535@gmail.com>
2019-08-02 14:41:41 +0800

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -36,10 +36,6 @@ export default {
       history.listen(location => {
         if (location.pathname === '/users') {
           dispatch({
-            type: 'querySuccess',
-            payload: {}
-          });
-          dispatch({
             type: 'query',
             payload: {}
           });
